fbc52fadcf78f0ff24ade3ed339b3aa224cf98aa
okorkov <alex.okorkov@gmail.com>
2021-02-18 14:30:38 -0800

    commit tree 15bcfec984ad6962026a647c3be23cda5424157d

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Your code here
+
+function mapToNegativize(array) {
+  for(let i = 0; i < array.length; i++) {
+    array[i] = array[i] * -1;
+  }
+  return array;
+}
+
+function mapToNoChange(array) {
+  return array;
+}
+
+function mapToDouble(array) {
+  for(let i = 0; i < array.length; i++) {
+    array[i] = array[i] * 2;
+  }
+  return array;
+}
+
+function mapToSquare(array) {
+  for(let i = 0; i < array.length; i++) {
+    array[i] = array[i] * array[i];
+  }
+  return array;
+}
+
+function reduceToTotal(array, startingPoint=0) {
+  for(let i = 0; i < array.length; i++) {
+    startingPoint += array[i];
+  }
+  return startingPoint;
+}
+
+function reduceToAllTrue(array) {
+  let result;
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] !== false) {
+      result = true;
+    } else {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+
+function reduceToAnyTrue(array) {
+  let result;
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] === true) {
+      result = true;
+      break;
+    } else {
+      result = false;
+    }
+  }
+  return result;
+}
